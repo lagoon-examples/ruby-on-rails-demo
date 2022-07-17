1c91de6718ca254858dfe3f5a31051fde2e685cb
Blaize Kaye <blaize.kaye@amazee.com>
2022-07-18 11:05:00 +1200

    Demonstrates Log integration

--- Gemfile
+++ Gemfile
@@ -47,6 +47,9 @@ gem "bootsnap", require: false
 # Use Sass to process CSS
 # gem "sassc-rails"
 
+
+gem 'logstash-logger'
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative "boot"
 
 require "rails/all"
 
+# require 'logstash-logger'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -11,18 +13,25 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
-    # config.hosts << IPAddr.new("0.0.0.0/0"),        # All IPv4 addresses.
-    # config.hosts << IPAddr.new("::/0"),             # All IPv6 addresses.
-
     # LAGOON: We bypass all host authorization since this is both dynamic
     # and taken care of by the infrastructure.
     config.hosts = nil
+
+    # LAGOON: Here we enable UDP logging if we're running in a Lagoon environment
+    # This is demonstrated by using the `logstash-logger` Gem.
+    # See https://docs.lagoon.sh/logging/logging/
+    if ENV.has_key?('LAGOON_PROJECT') && ENV.has_key?('LAGOON_ENVIRONMENT') then
+      lagoon_namespace = ENV['LAGOON_PROJECT'] + "-" + ENV['LAGOON_ENVIRONMENT']
+      LogStashLogger.configure do |config|
+        config.customize_event do |event|
+          event["type"] = lagoon_namespace
+        end
+      end
+
+      config.logstash.host = 'application-logs.lagoon.svc'
+      config.logstash.type = :udp
+      config.logstash.port = 5140
+    end
+    
   end
 end
